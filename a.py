4a99e44524fb0f76c80c18698cece04243f17ed7
TImovKud <119116581+TImovKud@users.noreply.github.com>
2023-02-24 20:00:29 +0400

    Update a.py

--- a.py
+++ a.py
@@ -1,12 +1,11 @@
 from tkinter import*
 
 def click():
-    messagebox.showinfo('Заголовок', '{username}'.format(username=username))
+    a=Label(root, text= width=15, bg='black', fg='cyan', font='consolas').pack
 
 root=Tk()
 root.title('Приложение')
 root.geometry('500x400')
-#a=Label(root, text= width=15, bg='black', fg='cyan', font='consolas').pack
 b=Entry(root, width=10, bg='white', fg='red', font='consolas').pack
 btn=Button(root, text='Click', width=15, height=2, bg='white', command=click).pack
 root.mainloop()
